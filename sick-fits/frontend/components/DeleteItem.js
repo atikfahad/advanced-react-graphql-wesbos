8f161dd8378d164dd570e056cf6f5be9836db57d
Atik Fahad <fahadatik@gmail.com>
2020-05-28 06:37:58 +0600

    remove deleted item from cache and reload cache apollo

--- sick-fits/frontend/components/DeleteItem.js
+++ sick-fits/frontend/components/DeleteItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { ALL_ITEMS_QUERY } from './Items';
 
 const DELETE_ITEM_MUTATION = gql`
   mutation DELETE_ITEM_MUTATION($id: ID!) {
@@ -11,11 +12,24 @@ const DELETE_ITEM_MUTATION = gql`
 `;
 
 export default class DeleteItem extends Component {
+  update = (cache, payload) => {
+    // manually update the cache on the client, so that it matches the server
+    // 1. Read the cache for the items we want
+    const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
+    console.log(data);
+    // 2. Filter the deleted item out of the page
+    data.items = data.items.filter(
+      (item) => item.id != payload.data.deleteItem.id
+    );
+    // 3. Put the items back!
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
+  };
   render() {
     return (
       <Mutation
         mutation={DELETE_ITEM_MUTATION}
         variables={{ id: this.props.id }}
+        update={this.update}
       >
         {(deleteItem, { error }) => (
           <button
