0ef9a915aeb4b612f396ed95429cec41f6a95ffc
Atik Fahad <fahadatik@gmail.com>
2020-05-29 07:01:05 +0600

    initial wrapper component for permission based auth

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,7 +1,12 @@
 import CreateItem from '../components/CreateItem';
+import PleaseSignin from '../components/PleaseSignin';
 import Link from 'next/link';
 const Sell = (props) => {
-  return <CreateItem />;
+  return (
+    <PleaseSignin>
+      <CreateItem />
+    </PleaseSignin>
+  );
 };
 
 export default Sell;
